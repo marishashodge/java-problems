7cae54704df68ee8df089bc186e5c881206871f1
Marisha Schumacher-Hodge <marisha.schumacher.hodge@gmail.com>
2016-05-25 00:12:32 -0700

    Added comments

--- ProjectEuler25.java
+++ ProjectEuler25.java
@@ -1,20 +1,22 @@
 public class ProjectEuler25 {
 
   public static void main(String args[]) {
-
+    // convert argument to integer
     String argString = args[0];
     int digits = Integer.parseInt(argString);
+
     if (digits == 1) {
         System.out.println(1);
     }
 
     else {
-
+      // we know the first two Fibonnacci numbers are 1
       int fPrev = 1;
       int fPrevPrev = 1;
-
       int index = 2;
 
+      // use a bottom-up approach to move up the Fibonnacci sequence until
+      // the digits argument matches the number of digits at a certain Fibonnacci sequence
       while (true) {
           int fNext = fPrev + fPrevPrev;
           fPrevPrev = fPrev;
@@ -28,8 +30,7 @@ public class ProjectEuler25 {
               break;
 
           }
-          }
       }
     }
-
   }
+}
